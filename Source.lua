c0e4b495246a0513e90b8421691db9762c0b41b7
Asu <sushy.die@gmail.com>
2025-01-11 15:52:29 +0100

    updated

--- Source.lua
+++ Source.lua
@@ -27,7 +27,25 @@ for _, v in ipairs(previewFolder:GetChildren()) do
     v:Destroy()
 end
 
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))() or loadstring(game:HttpGet('https://raw.githubusercontent.com/SiriusSoftwareLtd/Rayfield/ab965bf9a6f9890e6447c9b377678f5bffd8a379/source.lua'))()
+local Rayfield
+local success
+
+success, Rayfield = pcall(function()
+    return loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+end)
+
+if not success then
+    success, Rayfield = pcall(function()
+        return loadstring(game:HttpGet('https://raw.githubusercontent.com/SiriusSoftwareLtd/Rayfield/ab965bf9a6f9890e6447c9b377678f5bffd8a379/source.lua'))()
+    end)
+end
+
+if not success then
+    success, Rayfield = pcall(function()
+        return loadstring(game:HttpGet('https://github.com/SiriusSoftwareLtd/Rayfield/blob/28c7c270669c16a2ae1526eaaac1dbf927aa881e/source.lua'))()
+    end)
+end
+
 if game:GetService("CoreGui"):FindFirstChild("MSGISSUE") then
 game:GetService("CoreGui").MSGISSUE:Destroy()
 end
@@ -65,17 +83,19 @@ local Window = Rayfield:CreateWindow({
        RememberJoins = true
     },
  
-    KeySystem = false,
+    KeySystem = true,
     KeySettings = {
-       Title = "Untitled",
+       Title = "Build A Boat For Treasure",
        Subtitle = "Key System",
-       Note = "No method of obtaining the key is provided",
-       FileName = "Key",
-       SaveKey = true,
+       Note = "Key available in discord, link copied to clipboard",
+       FileName = "ASUBABFTKey",
+       SaveKey = false,
        GrabKeyFromSite = false,
-       Key = {"Hello"}
+       Key = {"terraria"} -- yes there is a keysystem but my script is not obfuscated lol
     }
 })
+
+setclipboard("https://discord.gg/zrAB2m5gvz")
 
 local Global = Window:CreateTab("Global", 125428076789049)
 local ImageLoader = Window:CreateTab("Image Loader", 91865122737183)
@@ -247,6 +267,23 @@ local Button = Global:CreateButton({
     Name = "Load Infinite Yield",
     Callback = function()
         loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+    end,
+})
+
+local Button = Global:CreateButton({
+    Name = "Tp tool",
+    Callback = function()
+        mouse = game.Players.LocalPlayer:GetMouse()
+        tool = Instance.new("Tool")
+        tool.RequiresHandle = false
+        tool.Name = "Tp tool"
+        tool.ToolTip = "Equip + click = tp"
+        tool.Activated:connect(function()
+        local pos = mouse.Hit+Vector3.new(0,2.5,0)
+        pos = CFrame.new(pos.X,pos.Y,pos.Z)
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = pos
+        end)
+        tool.Parent = game.Players.LocalPlayer.Backpack
     end,
 })
 
@@ -635,169 +672,7 @@ coroutine.wrap(function()
     end
 end)()
 
---loadstring(game:HttpGet('https://raw.githubusercontent.com/TheRealAsu/BABFT/refs/heads/main/BlockId'))()
-local BlockId = {
-    BalloonBlock = 1916437856,
-    BalloonStarBlock = 1973706944,
-    BrickBlock = 1608273751,
-    Button = 1678033905,
-    CaneBlock = 1298643792,
-    CaneRod = 1298644378,
-    Cannon = 845567732,
-    CarSeat = 1863051164,
-    Chair = 924419491,
-    ConcreteBlock = 845565990,
-    ConcreteRod = 845564596,
-    CornerWedge = 845567909,
-    FabricBlock = 1608274294,
-    FireworkD = 7036614604,
-    Flag = 845563550,
-    GlassBlock = 1335289047,
-    Glue = 1887147909,
-    GoldBlock = 1678364253,
-    Harpoon = 2062877865,
-    HugeMotor = 1865438463,
-    IceBlock = 1608273971,
-    Lever = 1608273289,
-    LifePreserver = 958894042,
-    MarbleBlock = 845566206,
-    MarbleRod = 845564866,
-    Mast = 845566917,
-    MegaThruster = 1358894694,
-    MetalBlock = 845565844,
-    MetalRod = 845564481,
-    Motor = 9236142098,
-    MysteryBox = 2035087825,
-    ObsidianBlock = 1335288552,
-    PlasticBlock = 1609332225,
-    Pumpkin = 1105248393,
-    RustedBlock = 845565648,
-    RustedRod = 845564347,
-    Seat = 845567578,
-    Servo = 1863050474,
-    Spring = 1863049770,
-    Star = 1916677740,
-    SteelIBeam = 845566665,
-    Step = 845568429,
-    StoneBlock = 845565497,
-    StoneRod = 845564162,
-    TNT = 932196135,
-    Throne = 845567243,
-    Thruster = 1317812037,
-    TitaniumBlock = 845566458,
-    TitaniumRod = 845565080,
-    Torch = 5717267458,
-    Truss = 845568199,
-    Wedge = 845568062,
-    Helm = 845567402,
-    Window = 845563704,
-    WinterThruster = 1298650848,
-    WoodBlock = 845568340,
-    WoodDoor = 1191997076,
-    WoodRod = 845563975,
-    WoodTrapDoor = 1191997319,
-    YellowChest = 976448763,
-    BouncyBlock = 2293816241,
-    Bread = 2102742548,
-    CandyBlue = 7781285156,
-    Plushie2 = 2214257779,
-    Plushie1 = 2223411329,
-    GrassBlock = 2417963634,
-    Lamp = 2413603467,
-    Candle = 2413603938,
-    ChestLegendary = 4717828937,
-    ChestRare = 4717827311,
-    ChestCommon = 4717826099,
-    ChestUncommon = 4717826702,
-    Cake = 2103921305,
-    CandyOrange = 7781288646,
-    CandyPurple = 7781287748,
-    ChestEpic = 4717828261,
-    SandBlock = 2418018500,
-    HalloweenThruster = 2501530509,
-    JetTurbineWinter = 2690396507,
-    NeonBlock = 2690438936,
-    JetTurbine = 2592852037,
-    PilotSeat = 2592852717,
-    SonicJetTurbine = 2592851747,
-    DualCaneHarpoon = 2690396999,
-    Firework = 2042685042,
-    FireworkB = 7036612976,
-    FireworkC = 7036613636,
-    SoccerBall = 2732318916,
-    BoxingGlove = 2783557827,
-    Heart = 2855511869,
-    CandyPink = 7781284023,
-    JetPack = 3230273281,
-    JetPackEaster = 3230273718,
-    Magnet = 2910074282,
-    CannonMount = 7130971602,
-    Gameboard = 3162472457,
-    GunMount = 7130971085,
-    SwordMount = 7130969623,
-    LockedDoor = 3162472006,
-    UltraThruster = 3164908588,
-    ShieldGenerator = 3162472660,
-    Piston = 3162469425,
-    SticksOfTNT = 2511283148,
-    CannonTurret = 3162469847,
-    Hinge = 3162470626,
-    TreasureSmall = 5176251125,
-    JetPackStar = 3268106680,
-    JetPackUltra = 3268107303,
-    Potions = 3500449318,
-    TreasureMedium = 5176250512,
-    HarpoonGold = 3583968049,
-    TreasureLarge = 5176249676,
-    Plushie3 = 3591541892,
-    Portal = 3744399826,
-    JetPackSteampunk = 3838566130,
-    BowMount = 7131030442,
-    KnightSwordMount = 7131029546,
-    LightningStaffMount = 7131031163,
-    JackOLantern = 4079113414,
-    PineTree = 4210890467,
-    CoalBlock = 4539749508,
-    Sign = 4539749166,
-    BoatMotorUltra = 4539748713,
-    BoatMotor = 4539748155,
-    BoatMotorWinter = 4539748452,
-    DragonEgg = 4742940542,
-    FrontWheel = 4736855340,
-    BackWheel = 4736853414,
-    FrontWheelCookie = 4803870316,
-    HugeBackWheel = 4742770672,
-    BackWheelCookie = 4803870924,
-    HugeFrontWheel = 4742773097,
-    Plushie4 = 4918988544,
-    Trophy1st = 5299317467,
-    Trophy2nd = 5299318083,
-    Trophy3rd = 5299319293,
-    HarpoonDragon = 5740994229,
-    BackWheelMint = 6228838828,
-    FrontWheelMint = 6228838214,
-    CameraDome = 6218312957,
-    Camera = 6218312341,
-    ToyBlock = 5578285243,
-    Switch = 6361970614,
-    LightBulb = 6635725107,
-    ParachuteBlock = 6635488100,
-    SwitchBig = 6828907824,
-    CannonEgg = 6568169777,
-    TrophyMaster = 6876313983,
-    FireworkA = 7036591081,
-    Delay = 7743806268,
-    CandyRed = 7781250539,
-    SnowballTurret = 8452611946,
-    CandyCaneSwordMount = 8452610423,
-    Note = 8452606673,
-    SmoothWoodBlock = 8142306398,
-    SpikeTrap = 7207314809,
-    MiniGun = 2302342262,
-    LaserTurret = 12229204385,
-    Bar = 16088076429,
-    Rope = 16088186920
-}
+local BlockId = loadstring(game:HttpGet('https://raw.githubusercontent.com/TheRealAsu/BABFT/refs/heads/main/BlockId.lua'))()
 
 local ImageFiles = {}
 
@@ -905,6 +780,7 @@ local batchSize = 700
 local TotalBlockInBlocksFolderBeforeBuildImageInitYesThisVarIsVeryLong = 0
 local USEURL = nil
 local TempData = {}
+getgenv().ImgLoaderStat = true
 
 local function LPTEAM2()
     local teamName = player.Team.Name
@@ -1074,17 +950,27 @@ local function buildImageFAST()
     end
 
     for i = 1, #parts do
+        if getgenv().ImgLoaderStat == false then
+            break
+        end
         task.spawn(function()
             local part = parts[i]
             if not part then
                 return
             end
-
+            if getgenv().ImgLoaderStat == false then
+                return
+            end
             local WORLDPOS = part.Position
             local partRot = part.CFrame - part.Position
-            local newCFrame = CFrame.new(math.random(-69, 69), math.random(120000, 2200000), math.random(-69, 69))
+            local newCFrame = CFrame.new(math.random(-69, 69), math.random(-2200000, -120000), math.random(-69, 69))
 
-            game:GetService("Players").LocalPlayer.Backpack.BuildingTool.RF:InvokeServer(
+            if getgenv().ImgLoaderStat == false then
+                return
+            end
+
+            local BuildPath = player.Backpack:FindFirstChild("BuildingTool") or player.Character:FindFirstChild("BuildingTool")
+            BuildPath.RF:InvokeServer(
                 BlockType,
                 uszLPBlockvalue,
                 workspace:FindFirstChild(Zonesss),
@@ -1096,24 +982,34 @@ local function buildImageFAST()
 
     local blocks = workspace.Blocks:FindFirstChild(LNplayer):GetChildren()
 
-    while #blocks ~= #parts do
-        task.wait(0.15)
+    while #blocks < #parts do
+        task.wait(0.5)
         blocks = workspace.Blocks:FindFirstChild(LNplayer):GetChildren()
     end
 
     for i = 1, #parts do
+        if getgenv().ImgLoaderStat == false then
+            break
+        end
         task.spawn(function()
             local part = parts[i]
             if not part then
                 return
             end
-
+            if getgenv().ImgLoaderStat == false then
+                return
+            end
             local WORLDPOS = part.Position
             local partRot = part.CFrame - part.Position
             local newwCFrame = CFrame.new(WORLDPOS) * partRot * CFrame.Angles(0, math.rad(90), 0)
             local targetBlock = blocks[TotalBlockInBlocksFolderBeforeBuildImageInitYesThisVarIsVeryLong + i]
 
-            game.Players.LocalPlayer.Backpack.ScalingTool.RF:InvokeServer(
+            if getgenv().ImgLoaderStat == false then
+                return
+            end
+            
+            local ScalePath = player.Backpack:FindFirstChild("ScalingTool") or player.Character:FindFirstChild("ScalingTool")
+            ScalePath.RF:InvokeServer(
                 targetBlock,
                 Vector3.new(Bdepth, blockSize, blockSize),
                 newwCFrame
@@ -1130,20 +1026,48 @@ local function buildImageFAST()
         })
     end
     
-    game:GetService("Players").LocalPlayer.Backpack.PaintingTool.RF:InvokeServer(paintData)
+    local PaintPath = player.Backpack:FindFirstChild("PaintingTool") or player.Character:FindFirstChild("PaintingTool")
+    PaintPath.RF:InvokeServer(paintData)
 
     for _, part in ipairs(folder:GetChildren()) do
         part:destroy()
     end
-    
-    task.delay(1, function()
-        Rayfield:Notify({
-            Title = "Image successfully loaded",
-            Content = "You can now use your inventory again",
-            Duration = 6.5,
-            Image = 124144713366592,
-        })
-    end)
+
+    if getgenv().ImgLoaderStat == false then
+        task.delay(1, function()
+            Rayfield:Notify({
+                Title = "Image aborted",
+                Content = "All unplaced blocks will be deleted”",
+                Duration = 6.5,
+                Image = 124144713366592,
+            })
+        end)
+    else
+        task.delay(1, function()
+            Rayfield:Notify({
+                Title = "Image successfully loaded",
+                Content = "You can now use your inventory again",
+                Duration = 6.5,
+                Image = 124144713366592,
+            })
+        end)
+    end
+
+    local remote = player.Character:FindFirstChild("DeleteTool") or player.Backpack:FindFirstChild("DeleteTool")
+
+    for i = 1, #blocks do
+        task.spawn(function()
+            local block = blocks[i]
+            if not block then
+                return
+            end
+
+            local blockY = block.PrimaryPart and block.PrimaryPart.Position.Y or nil
+            if blockY and blockY >= -2200000 and blockY <= -120000 then
+                    remote.RF:InvokeServer(blocks[i])
+            end
+        end)
+    end    
 end
 
 function onImgBlockSecChanged()
@@ -1251,7 +1175,7 @@ local Input = ImageLoader:CreateInput({
 
         ImageLoaderFile:Set("Fetching...", 72272740678757, Color3.fromRGB(121, 188, 226), false)
 
-        if string.sub(Text, 1, 5) == "https" then
+        if string.sub(Text, 1, 6) == "https:" then
             ImageLoaderFile:Set("Method: URL | Status: Fetching...", 110690411966110, Color3.fromRGB(121, 188, 226), false)
 
             local url = "https://therealasu.pythonanywhere.com/process_image" -- It is useless to DDOS it, all you're going to do is DDOS pythonanywhere which are protected against that, and the server doesn't cost me anything, it's free
@@ -1305,7 +1229,7 @@ local Input = ImageLoader:CreateInput({
                         ImageLoaderFile:Set("Method: URL | Status: Error", 110690411966110, Color3.fromRGB(255, 66, 41), false)
                         Rayfield:Notify({
                             Title = "Error",
-                            Content = "API Error: Url not supported",
+                            Content = "The API may be overloaded OR Url not supported , there's an alternative and a easy fix tutorial in the Discord server",
                             Duration = 6.5,
                             Image = 124144713366592,
                         })
@@ -1344,7 +1268,7 @@ local Input = ImageLoader:CreateInput({
                 ImageLoaderFile:Set("Method: URL | Status: Error", 110690411966110, Color3.fromRGB(255, 66, 41), false)
                 Rayfield:Notify({
                     Title = "Error",
-                    Content = "The API may be overloaded, there's an alternative in the Discord server",
+                    Content = "The API may be overloaded OR Url not supported , there's an alternative and a easy fix tutorial in the Discord server",
                     Duration = 6.5,
                     Image = 124144713366592,
                 })
@@ -1497,15 +1421,14 @@ local Slider = ImageLoader:CreateSlider({
     end,
 })
 
- local Slider = ImageLoader:CreateSlider({
-    Name = "Move",
-    Range = {0, 100},
-    Increment = 0.1,
-    Suffix = "stud",
-    CurrentValue = 45,
-    Flag = "",
-    Callback = function(Value)
-        Unit = Value
+ local Input = ImageLoader:CreateInput({
+    Name = "Move multiplier",
+    CurrentValue = "45",
+    PlaceholderText = "number",
+    RemoveTextAfterFocusLost = false,
+    Flag = "Input1",
+    Callback = function(Text)
+        Unit = tostring(Text)
     end,
  })
 
@@ -1648,8 +1571,8 @@ local ImgStatsP = ImageLoader:CreateParagraph({Title = "Information", Content = 
     Name = "Build Image | Preview must be enabled",
     Callback = function()
         Rayfield:Notify({
-            Title = "Load Image",
-            Content = "Tip: open your inventory for aborting",
+            Title = "Image Loader",
+            Content = "DO NOT place blocks until finished",
             Duration = 10,
             Image = 124144713366592,
          })
@@ -1687,13 +1610,27 @@ else
             Image = 124144713366592,
          })
 end
+        getgenv().ImgLoaderStat = true
         task.spawn(buildImageFAST)
+    end,
+ })
+
+ local Button = ImageLoader:CreateButton({
+    Name = "Abort",
+    Callback = function()
+        getgenv().ImgLoaderStat = false
+        Rayfield:Notify({
+            Title = "Abortion request",
+            Content = "the next task will not be applied, note that this button is only useful during the task where blocks are loaded",
+            Duration = 6.5,
+            Image = 124144713366592,
+        })
     end,
  })
 
 local Label = ImageLoader:CreateLabel("Building speed will also depend on your ping. You can crash if your wifi speed is too slow.", 134637165939940, Color3.fromRGB(204, 156, 0), true)
 
-local Label = ImageLoader:CreateLabel("inventory must be closed during the entire process for it to work. open inventory to stop the process.", 134637165939940, Color3.fromRGB(204, 156, 0), true)
+local Label = ImageLoader:CreateLabel("the image may not load if many blocks have already been placed, I have to fix this", 134637165939940, Color3.fromRGB(204, 156, 0), true)
 
 function ImgStats()
     local startTime = tick()
@@ -1948,11 +1885,6 @@ local Button = Miscellaneous:CreateButton({
  local Button = Miscellaneous:CreateButton({
     Name = "Color all blocks",
     Callback = function()
-        local args = {
-            [1] = "ShareBlocks",
-            [2] = true
-        }
-        workspace.SettingFunction:InvokeServer(unpack(args))
         local playerteam = player.Team.Name
         local blocktoget = game:GetService("Teams"):FindFirstChild(playerteam).TeamLeader.Value
         local playerFolder = game.Workspace.Blocks:FindFirstChild(blocktoget)
@@ -1988,11 +1920,6 @@ local Toggle = Miscellaneous:CreateToggle({
     CurrentValue = false,
     Flag = "Toggle1",
     Callback = function(Value)
-        local args = {
-            [1] = "ShareBlocks",
-            [2] = true
-        }
-        workspace.SettingFunction:InvokeServer(unpack(args))
 while Value do
     if Value then
     local playerteam = player.Team.Name
@@ -2028,7 +1955,7 @@ end
     end,
  })
 
- local FStats = Miscellaneous:CreateParagraph({Title = "Info", Content = "The button above will colors all the player's blocks in sharing mode with random colors (it will color the team leader's blocks, it also works when you are the leader), if it doesn't stop, use the paint tool, it will stop the script"})
+ local FStats = Miscellaneous:CreateParagraph({Title = "Info", Content = "The button above will colors all the player's blocks with random colors (if sharing mode enable, it will color the team leader's blocks, it also works when you are the leader), if it doesn't stop, use the paint tool, it will stop the script"})
 
  local function removeLock()
     local Teams = {"BlackZone", "CamoZone", "MagentaZone", "New YellerZone", "Really blueZone", "Really redZone", "WhiteZone"}
@@ -2258,7 +2185,7 @@ coroutine.resume(ImgStats)
 coroutine.resume(initimgfiles)
 end
 
-local Button = Miscellaneous:CreateButton({
+zdzddz = Miscellaneous:CreateButton({
     Name = "Halloween [maybe need an anti-kick]",
     Callback = function()
         TeleportService:Teleport(1930665568, game.Players.LocalPlayer) -- WARNING: YOU WILL GET KICKED WHEN YOU JOIN IT, need to try with a client-sided anti kick
