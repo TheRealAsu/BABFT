5824e040bad6cd3dd62fab30f1d76deb99b4b8d7
Asu <sushy.die@gmail.com>
2025-02-02 22:01:40 +0100

    MessageIssue.lua

--- MessageIssue.lua
+++ MessageIssue.lua
@@ -41,7 +41,7 @@ G2L["6"] = Instance.new("LocalScript", G2L["5"]);
 local function C_6()
 local script = G2L["6"];
 	local textLabel = script.Parent
-	local fullText = "New script is available on the discord. This one is outdated."
+	local fullText = "New script is currently down. This one is the backup one."
 	textLabel.Text = ""
 	local delayTime = 0.05 
 	for i = 1, #fullText do
